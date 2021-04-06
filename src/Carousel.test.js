a817f6eacd1375a22e02b940dd5903cc5ad16ae9
Tim Birkmire <timbirkmire@yahoo.com>
2021-04-06 18:31:32 -0400

    Hide left and right arrow buttons on first/last photo and write tests for both

--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -38,12 +38,16 @@ it('works when you click on the right arrow', function () {
 it('works when you click on the left arrow', function () {
   const { queryByTestId, queryByAltText } = render(<Carousel />);
 
+  // move forward in the carousel (to start at second photo)
+  const rightArrow = queryByTestId('right-arrow');
+  fireEvent.click(rightArrow);
+
   // expect the first image to show, but not the third
   expect(
-    queryByAltText('Photo by Richard Pasquarella on Unsplash')
+    queryByAltText('Photo by Pratik Patel on Unsplash')
   ).toBeInTheDocument();
   expect(
-    queryByAltText('Photo by Josh Post on Unsplash')
+    queryByAltText('Photo by Richard Pasquarella on Unsplash')
   ).not.toBeInTheDocument();
 
   // move backward in the carousel
@@ -52,7 +56,51 @@ it('works when you click on the left arrow', function () {
 
   // expect the third image to show, but not the first
   expect(
-    queryByAltText('Photo by Richard Pasquarella on Unsplash')
+    queryByAltText('Photo by Pratik Patel on Unsplash')
   ).not.toBeInTheDocument();
+  expect(
+    queryByAltText('Photo by Richard Pasquarella on Unsplash')
+  ).toBeInTheDocument();
+});
+
+it('hides the left arrow on the first card in the array', function () {
+  const { queryByTestId, queryByAltText } = render(<Carousel />);
+
+  // expect the left arrow to not show for the first, but show for the second
+  expect(
+    queryByAltText('Photo by Richard Pasquarella on Unsplash')
+  ).toBeInTheDocument();
+  expect(queryByTestId('left-arrow')).not.toBeInTheDocument();
+
+  // move forward in the carousel
+  const rightArrow = queryByTestId('right-arrow');
+  fireEvent.click(rightArrow);
+
+  // expect the second image to show, but not the first
+  expect(
+    queryByAltText('Photo by Pratik Patel on Unsplash')
+  ).toBeInTheDocument();
+  expect(queryByTestId('left-arrow')).toBeInTheDocument();
+});
+
+it('hides the right arrow on the last card in the array', function () {
+  const { queryByTestId, queryByAltText } = render(<Carousel />);
+  // move forward to end of the carousel
+  const rightArrow = queryByTestId('right-arrow');
+  fireEvent.click(rightArrow);
+  fireEvent.click(rightArrow);
+
+  // expect the right arrow to not show for the last, but show for the second
   expect(queryByAltText('Photo by Josh Post on Unsplash')).toBeInTheDocument();
+  expect(queryByTestId('right-arrow')).not.toBeInTheDocument();
+
+  // move backward one in the carousel
+  const leftArrow = queryByTestId('left-arrow');
+  fireEvent.click(leftArrow);
+
+  // expect the second image to show, but not the first
+  expect(
+    queryByAltText('Photo by Pratik Patel on Unsplash')
+  ).toBeInTheDocument();
+  expect(queryByTestId('right-arrow')).toBeInTheDocument();
 });
